e26b1356d691f550eae5648a54a1db174ac0c743
Brandon Yuen <brandonyuen2001@gmail.com>
2021-01-09 14:06:13 -0800

    created readyDiscord function

--- courseBot.js
+++ courseBot.js
@@ -1,1 +1,11 @@
-console.log()
+console.log('Loading Bot');
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.login('Nzk3NTc0NjU0MDg4MTgzODM5.X_odRw.l9uCqUCihnaMfrOi7OEfTor8gf0')
+
+client.on('ready', readyDiscord);
+
+function readyDiscord() {
+    console.log('Bot is Ready');
+}
